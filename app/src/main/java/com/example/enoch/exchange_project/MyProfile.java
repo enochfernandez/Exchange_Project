53f0ddef9e52d251ad1f3b3e510f80b4e7fc1b49
JCTubio <juancruztubio@gmail.com>
2016-06-05 16:06:31 +0200

    Added Profile, edit profile and edit contact info (Only design, no java code)

---
+++ app/src/main/java/com/example/enoch/exchange_project/MyProfile.java
@@ -0,0 +1,22 @@
+package com.example.enoch.exchange_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MyProfile extends AppCompatActivity {
+
+    Button skillsButton;
+    Button profileButton;
+    EditText bio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+        bio = (EditText) findViewById(R.id.bioText);
+        skillsButton = (Button) findViewById(R.id.skillsButtonProfile);
+        profileButton = (Button) findViewById(R.id.editContactInfoButtonProfile);
+    }
+}
